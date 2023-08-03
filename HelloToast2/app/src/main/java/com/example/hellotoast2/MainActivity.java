73aaf3a2c0ad2d95d0aca1ab34581674c3b5d263
xuzepei <xuzepei@gmail.com>
2023-08-03 09:11:02 +0800

    no message

--- HelloToast2/app/src/main/java/com/example/hellotoast2/MainActivity.java
+++ HelloToast2/app/src/main/java/com/example/hellotoast2/MainActivity.java
@@ -1,36 +1,37 @@
-package com.example.appdemo;
+package com.example.hellotoast2;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    private int count = 0;
+    int count = 0;
     private TextView countTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        countTV = (TextView)findViewById(R.id.show_count);
-
-        Log.d("MainActivity","Hello, world");
+        countTV = (TextView) findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        Log.d("MainActivity", "Clicked show toast button");
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
         toast.show();
     }
 
     public void countUp(View view) {
         count++;
-        if (countTV != null) {
+        if(countTV != null) {
             countTV.setText(Integer.toString(count));
         }
     }
